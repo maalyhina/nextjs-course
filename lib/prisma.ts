9f4324de045e4961cc1dad708e1d71ab4b18b6dc
Малихіна Ангеліна Олегівна <vt231_mao@student.ztu.edu.ua>
2026-04-23 16:34:44 +0300

    fix prisma completely

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,18 +1,12 @@
-import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
-import { Pool } from "pg";
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient };
+const globalForPrisma = global as unknown as {
+  prisma: PrismaClient;
+};
 
-function createPrismaClient() {
-  const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  });
-  const adapter = new PrismaPg(pool);
-  return new PrismaClient({ adapter } as any);
-}
-
-export const prisma = globalForPrisma.prisma || createPrismaClient();
+export const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
